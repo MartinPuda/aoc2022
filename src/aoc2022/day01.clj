387cf85227ecfbc7cef5035feb47e6a90a89c02c
MartinPuda <mapuda@seznam.cz>
2022-12-01 13:07:47 +0100

    day01

---
+++ src/aoc2022/day01.clj
@@ -0,0 +1,29 @@
+(ns aoc2022.day01
+  (:require [clojure.string :as s])
+  (:gen-class))
+
+(defn block->calories [block]
+  (transduce (map parse-long) + (s/split-lines block)))
+
+(def data
+  (->> (s/split (slurp "resources/input01.txt") #"\n\n")
+       (map block->calories)))
+
+(defn part1 [data]
+  (reduce max data))
+
+(defn part2 [data]
+  (->> data
+       (sort >)
+       (take 3)
+       (reduce +)))
+
+;;;;;
+
+(defn tests []
+  (assert (= (part1 data) 68467))
+  (assert (= (part2 data) 203420)))
+
+(tests)
+; 68467
+;203420
